e48927a9323571800e1763dd9cd64cd3cbf67ed8
nikolay <nikolay.li2008@gmail.com>
2026-04-13 00:26:28 -0700

    fixed url to id

--- eventutils/docs_url_to_id.go
+++ eventutils/docs_url_to_id.go
@@ -3,10 +3,12 @@ package eventutils
 import "strings"
 
 // https://docs.google.com/document/d/id-example/edit?tab=t.0
+// https://docs.google.com/document/u/0/d/1x8B8h9ZFNIUcartcK7JLDUHjmnMTu62LP8hNzK82xgI/mobilebasic
 // extracts a Google docs id from the url
 // splits it at /d/ and gets the second part, then splits it at /edit and gets the first one
 func DocsUrlToId(url string) string {
 	id := strings.Split(url, "/d/")[1]
 	id = strings.Split(id, "/edit")[0]
+	id = strings.Split(id, "/mobilebasic")[0]
 	return id
 }
--- genericutils/format_id.go
+++
@@ -1,7 +0,0 @@
-package genericutils
-
-// returns the id of a url of a google docs doc
-func FormatID(unformatted string) string {
-	// split at /d/ and /edit
-	return ""
-}
